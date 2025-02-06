1721d0d1ae74e274f1ced17682ce8d1e922eaec9
chiaralle <chiaraallegrino@gmail.com>
2025-02-06 12:41:13 +0100

    add: names and hobbies

--- js/hobbies.js
+++ js/hobbies.js
@@ -1,5 +1,13 @@
-/*
-2 - hobbies.js
-1. Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire  un array di stringhe contenenti gli hobby.
-2. Esporta la funzione dal file.
-*/
+
+//2 - hobbies.js
+
+//1. Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. 
+function createHobbies(hobbyOne, hobbyTwo, hobbyThree ) {
+
+    //La funzione dovrebbe restituire  un array di stringhe contenenti gli hobby.
+    return [hobbyOne, hobbyTwo, hobbyThree];
+      
+  }
+
+//2. Esporta la funzione dal file.
+module.exports = createHobbies;
--- js/names.js
+++ js/names.js
@@ -1,6 +1,15 @@
-/*
-1 - names.js
-1. Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
-2. Esporta la funzione dal file.
-*/
 
+//1 - names.js
+ 
+//1. Crea una funzione che accetta due parametri: firstName, lastName.
+function createNames(firstName, lastName) {
+
+    //La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+    return {
+        firstName: firstName,
+        lastName: lastName 
+    };
+  }
+
+  //2. Esporta la funzione dal file.
+module.exports = createNames;
